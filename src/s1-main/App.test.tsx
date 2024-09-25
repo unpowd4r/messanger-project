440e480d36532166ee28d1cc98d9cd3fb9e27671
unpowd4r <sergay.frolov@yandex.ru>
2024-09-25 16:11:58 +0400

    hw4 completed

--- src/s1-main/App.test.tsx
+++ src/s1-main/App.test.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import {render, screen} from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from './App'
 
 test('renders learn react link', () => {
-    render(<App/>)
-    // const linkElement = screen.getByText(/learn react/i)
-    // expect(linkElement).toBeInTheDocument()
-    expect(1).toBe(1)
+	render(<App />)
+	// const linkElement = screen.getByText(/learn react/i)
+	// expect(linkElement).toBeInTheDocument()
+	expect(1).toBe(1)
 })
