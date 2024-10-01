a76bba8f260366dd703c52adc353aeded6c453e2
unpowd4r <sergay.frolov@yandex.ru>
2024-10-01 15:23:00 +0400

    hw5 refactor

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,8 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Error404 from './pages/Error404'
+import Junior from './pages/Junior'
+import JuniorPlus from './pages/JuniorPlus'
+import PreJunior from './pages/PreJunior'
 
 export const PATH = {
 	PRE_JUNIOR: '/pre-junior',
@@ -15,9 +18,9 @@ function Pages() {
 				<Route path='/' element={<Navigate to={PATH.PRE_JUNIOR} />} />
 
 				{/*роуты для /pre-junior, /junior, /junior-plus*/}
-				<Route path={PATH.PRE_JUNIOR} />
-				<Route path={PATH.JUNIOR} />
-				<Route path={PATH.JUNIOR_PLUS} />
+				<Route path={PATH.PRE_JUNIOR} element={<PreJunior />} />
+				<Route path={PATH.JUNIOR} element={<Junior />} />
+				<Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus />} />
 
 				{/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
 				<Route path='*' element={<Error404 />} />
