e8647a2a98bed63ddb91ebc24a95948bc9cc3601
unpowd4r <sergay.frolov@yandex.ru>
2024-10-10 15:39:19 +0400

    hw8 completed

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,26 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+	| { type: 'sort'; payload: 'up' | 'down' }
+	| { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (
+	state: UserType[],
+	action: ActionType
+): UserType[] => {
+	// need to fix any
+	switch (action.type) {
+		case 'sort': {
+			// by name
 
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
-    }
+			return action.payload === 'up'
+				? state.sort((a, b) => (a.name > b.name ? 1 : -1))
+				: state.sort((a, b) => (a.name < b.name ? 1 : -1))
+		}
+		case 'check': {
+			return state.filter(student => student.age > action.payload)
+		}
+		default:
+			return state
+	}
 }
