8c6d1ac0971b70ae1dc6d250b9f9abddd9ce7454
unpowd4r <sergay.frolov@yandex.ru>
2024-11-26 23:58:42 +0400

    hw15

--- src/s2-homeworks/hw15/HW15.tsx
+++ src/s2-homeworks/hw15/HW15.tsx
@@ -1,137 +1,142 @@
-import React, {useEffect, useState} from 'react'
-import s2 from '../../s1-main/App.module.css'
-import s from './HW15.module.css'
+import { Container } from '@mui/material'
 import axios from 'axios'
-import SuperPagination from './common/c9-SuperPagination/SuperPagination'
-import {useSearchParams} from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import s2 from '../../s1-main/App.module.css'
 import SuperSort from './common/c10-SuperSort/SuperSort'
+import SuperPagination from './common/c9-SuperPagination/SuperPagination'
+import s from './HW15.module.css'
 
 /*
-* 1 - дописать SuperPagination
-* 2 - дописать SuperSort
-* 3 - проверить pureChange тестами
-* 3 - дописать sendQuery, onChangePagination, onChangeSort в HW15
-* 4 - сделать стили в соответствии с дизайном
-* 5 - добавить HW15 в HW5/pages/JuniorPlus
-* */
+ * 1 - дописать SuperPagination
+ * 2 - дописать SuperSort
+ * 3 - проверить pureChange тестами
+ * 3 - дописать sendQuery, onChangePagination, onChangeSort в HW15
+ * 4 - сделать стили в соответствии с дизайном
+ * 5 - добавить HW15 в HW5/pages/JuniorPlus
+ * */
 
 type TechType = {
-    id: number
-    tech: string
-    developer: string
+	id: number
+	tech: string
+	developer: string
 }
 
 type ParamsType = {
-    sort: string
-    page: number
-    count: number
+	sort: string
+	page: number
+	count: number
 }
 
 const getTechs = (params: ParamsType) => {
-    return axios
-        .get<{ techs: TechType[], totalCount: number }>(
-            'https://samurai.it-incubator.io/api/3.0/homework/test3',
-            {params}
-        )
-        .catch((e) => {
-            alert(e.response?.data?.errorText || e.message)
-        })
+	return axios
+		.get<{ techs: TechType[]; totalCount: number }>(
+			'https://samurai.it-incubator.io/api/3.0/homework/test3',
+			{ params }
+		)
+		.catch(e => {
+			alert(e.response?.data?.errorText || e.message)
+		})
 }
 
 const HW15 = () => {
-    const [sort, setSort] = useState('')
-    const [page, setPage] = useState(1)
-    const [count, setCount] = useState(4)
-    const [idLoading, setLoading] = useState(false)
-    const [totalCount, setTotalCount] = useState(100)
-    const [searchParams, setSearchParams] = useSearchParams()
-    const [techs, setTechs] = useState<TechType[]>([])
+	const [sort, setSort] = useState('')
+	const [page, setPage] = useState(1)
+	const [count, setCount] = useState(4)
+	const [idLoading, setLoading] = useState(false)
+	const [totalCount, setTotalCount] = useState(100)
+	const [searchParams, setSearchParams] = useSearchParams()
+	const [techs, setTechs] = useState<TechType[]>([])
 
-    const sendQuery = (params: any) => {
-        setLoading(true)
-        getTechs(params)
-            .then((res) => {
-                // делает студент
+	const sendQuery = (params: ParamsType) => {
+		setLoading(true)
+		getTechs(params).then(res => {
+			if (res) {
+				setTechs(res.data.techs)
+				setTotalCount(res.data.totalCount)
+			}
+			setLoading(false)
+		})
+	}
 
-                // сохранить пришедшие данные
+	const onChangePagination = (newPage: number, newCount: number) => {
+		// делает студент
+		setPage(newPage)
+		setCount(newCount)
+		sendQuery({ page: newPage, count: newCount, sort })
+		setSearchParams({ page: newPage.toString(), count: newCount.toString() })
+	}
 
-                //
-            })
-    }
+	const onChangeSort = (newSort: string) => {
+		// делает студент
+		setSort(newSort)
+		setPage(1)
+		sendQuery({ page: 1, count, sort: newSort })
+		setSearchParams({ page: '1', count: count.toString(), sort: newSort })
+	}
 
-    const onChangePagination = (newPage: number, newCount: number) => {
-        // делает студент
+	useEffect(() => {
+		const params = Object.fromEntries(searchParams)
+		sendQuery({
+			page: +params.page || 1,
+			count: +params.count || 4,
+			sort: params.sort || '',
+		})
+		setPage(+params.page || 1)
+		setCount(+params.count || 4)
+	}, [searchParams])
 
-        // setPage(
-        // setCount(
+	const mappedTechs = techs.map(t => (
+		<div key={t.id} className={s.row}>
+			<div id={'hw15-tech-' + t.id} className={s.tech}>
+				{t.tech}
+			</div>
 
-        // sendQuery(
-        // setSearchParams(
+			<div id={'hw15-developer-' + t.id} className={s.developer}>
+				{t.developer}
+			</div>
+		</div>
+	))
 
-        //
-    }
+	return (
+		<div id={'hw15'}>
+			<div className={s2.hwTitle}>Homework #15</div>
+			<Container>
+				<div className={s2.hw}>
+					{idLoading && (
+						<div id={'hw15-loading'} className={s.loading}>
+							Loading...
+						</div>
+					)}
 
-    const onChangeSort = (newSort: string) => {
-        // делает студент
+					<SuperPagination
+						page={page}
+						itemsCountForPage={count}
+						totalCount={totalCount}
+						onChange={onChangePagination}
+					/>
 
-        // setSort(
-        // setPage(1) // при сортировке сбрасывать на 1 страницу
+					<div className={s.rowHeader}>
+						<div className={s.techHeader}>
+							tech
+							<SuperSort sort={sort} value={'tech'} onChange={onChangeSort} />
+						</div>
 
-        // sendQuery(
-        // setSearchParams(
+						<div className={s.developerHeader}>
+							developer
+							<SuperSort
+								sort={sort}
+								value={'developer'}
+								onChange={onChangeSort}
+							/>
+						</div>
+					</div>
 
-        //
-    }
-
-    useEffect(() => {
-        const params = Object.fromEntries(searchParams)
-        sendQuery({page: params.page, count: params.count})
-        setPage(+params.page || 1)
-        setCount(+params.count || 4)
-    }, [])
-
-    const mappedTechs = techs.map(t => (
-        <div key={t.id} className={s.row}>
-            <div id={'hw15-tech-' + t.id} className={s.tech}>
-                {t.tech}
-            </div>
-
-            <div id={'hw15-developer-' + t.id} className={s.developer}>
-                {t.developer}
-            </div>
-        </div>
-    ))
-
-    return (
-        <div id={'hw15'}>
-            <div className={s2.hwTitle}>Homework #15</div>
-
-            <div className={s2.hw}>
-                {idLoading && <div id={'hw15-loading'} className={s.loading}>Loading...</div>}
-
-                <SuperPagination
-                    page={page}
-                    itemsCountForPage={count}
-                    totalCount={totalCount}
-                    onChange={onChangePagination}
-                />
-
-                <div className={s.rowHeader}>
-                    <div className={s.techHeader}>
-                        tech
-                        <SuperSort sort={sort} value={'tech'} onChange={onChangeSort}/>
-                    </div>
-
-                    <div className={s.developerHeader}>
-                        developer
-                        <SuperSort sort={sort} value={'developer'} onChange={onChangeSort}/>
-                    </div>
-                </div>
-
-                {mappedTechs}
-            </div>
-        </div>
-    )
+					{mappedTechs}
+				</div>
+			</Container>
+		</div>
+	)
 }
 
 export default HW15
--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -6,49 +6,44 @@ const upIcon = '[/\\]'
 const noneIcon = '[--]'
 
 export type SuperSortPropsType = {
-    id?: string
-    sort: string
-    value: string
-    onChange: (newSort: string) => void
+	id?: string
+	sort: string
+	value: string
+	onChange: (newSort: string) => void
 }
 
 export const pureChange = (sort: string, down: string, up: string) => {
-    // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
-    return up // исправить
+	if (sort === '') return down // если нет сортировки, ставим down
+	if (sort === down) return up // если уже down, ставим up
+	if (sort === up) return '' // если уже up, сбрасываем
+	return ''
 }
 
-const SuperSort: React.FC<SuperSortPropsType> = (
-    {
-        sort, value, onChange, id = 'hw15',
-    }
-) => {
-    const up = '0' + value
-    const down = '1' + value
+const SuperSort: React.FC<SuperSortPropsType> = ({
+	sort,
+	value,
+	onChange,
+	id = 'hw15',
+}) => {
+	const up = '0' + value
+	const down = '1' + value
 
-    const onChangeCallback = () => {
-        onChange(pureChange(sort, down, up))
-    }
+	const onChangeCallback = () => {
+		onChange(pureChange(sort, down, up))
+	}
 
-    const icon = sort === down
-        ? downIcon
-        : sort === up
-            ? upIcon
-            : noneIcon
+	const icon = sort === down ? downIcon : sort === up ? upIcon : noneIcon
 
-    return (
-        <span
-            id={id + '-sort-' + value}
-            onClick={onChangeCallback}
-        >
-            {/*сделать иконку*/}
-            {/*<img*/}
-            {/*    id={id + '-icon-' + sort}*/}
-            {/*    src={icon}*/}
-            {/*/>*/}
-
-            {icon} {/*а это убрать*/}
-        </span>
-    )
+	return (
+		<span id={id + '-sort-' + value} onClick={onChangeCallback}>
+			{/*сделать иконку*/}
+			{/*<img*/}
+			{/*    id={id + '-icon-' + sort}*/}
+			{/*    src={icon}*/}
+			{/*/>*/}
+			{icon} {/*а это убрать*/}
+		</span>
+	)
 }
 
 export default SuperSort
